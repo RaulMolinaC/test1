55a53a782cb17596ec750555c34e12b39ad7bf42
RaulMolinaC <72096156+RaulMolinaC@users.noreply.github.com>
2020-10-01 08:37:22 +0200

    Creo que por fin está arreglado...

--- SolveEquation/Ecuación 2º Grado/Program.cs
+++ SolveEquation/Ecuación 2º Grado/Program.cs
@@ -5,9 +5,9 @@ namespace Ecuación_2º_Grado
 {
     class Program
     {
-       
 
 
+        
         static void Main(string[] args)
         {
 
@@ -16,21 +16,17 @@ namespace Ecuación_2º_Grado
             s1 = EquationSolvers.SolveEquation(2.0, -4.0);
 
             System.Console.WriteLine("s1 = " + s1);
-        
-        
+
+
+
+            double s2;
+
+            s2 = EquationSolvers.SolveEquation2(1.0, -1.0, 0.0);
+
+            System.Console.WriteLine("s2 " + s2);
+
         }
 
-
-
-        static void Main2(string[] args)
-        {
-            double s1;
-
-            s1 = EquationSolvers.SolveEquation2(1.0, -1.0, 0.0);
-            
-            System.Console.WriteLine("s1 " + s1);
-                    
-        }
 
 
 
