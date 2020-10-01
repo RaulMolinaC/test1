cee324fbf06b93f736d532af5be88b6183d6d900
RaulMolinaC <72096156+RaulMolinaC@users.noreply.github.com>
2020-10-01 08:15:59 +0200

    Save All

---
+++ HelloWorld (Terminado)/HelloWorld/Program.cs
@@ -0,0 +1,69 @@
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, x1, aux, aux2;
+
+            a = 3.0;
+
+            b = 7.0;
+
+            c = -10.0;
+
+            aux = (b * b) - (4 * a * c);
+
+            aux2 = System.Math.Sqrt(aux);
+
+            x1 = (-b + aux2) / (2 * a);
+
+            System.Console.WriteLine("Solución de x1 = " + x1);
+
+
+
+
+            double x2, aux3, aux4;
+
+            aux3 = (b * b) - (4 * a * c);
+
+            aux4 = System.Math.Sqrt(aux3);
+
+            x2 = (-b - aux4) / (2 * a);
+
+            System.Console.WriteLine("Solución de x2 = " + x2);
+
+
+
+
+            double d, h, m, s, horas, minutos, segundos;
+
+            d = 7.0;
+
+            h = 24.0;
+                        
+            m = 60.0;
+                       
+            s = 60.0;
+
+
+            horas = d * h;
+
+            minutos = horas * m;
+
+            segundos = minutos * s;
+
+
+            System.Console.WriteLine("7 días son : " + horas + " horas"); 
+
+            System.Console.WriteLine("7 días son : " + minutos + " minutos");
+
+            System.Console.WriteLine("7 días son : " + segundos + " segundos");
+
+
+
+             
+        }
+    }
+}
---
+++ SolveEquation/Ecuación 2º Grado/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ecuación_2º_Grado
+{
+    class Program
+    {
+       
+
+
+        static void Main(string[] args)
+        {
+
+            double s1;
+
+            s1 = EquationSolvers.SolveEquation(2.0, -4.0);
+
+            System.Console.WriteLine("s1 = " + s1);
+        
+        
+        }
+
+
+
+        static void Main2(string[] args)
+        {
+            double s1;
+
+            s1 = EquationSolvers.SolveEquation2(1.0, -1.0, 0.0);
+            
+            System.Console.WriteLine("s1 " + s1);
+                    
+        }
+
+
+
+    }
+}
