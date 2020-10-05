534923ce07f4f35cc19af3af7757c3df92bce383
RaulMolinaC <72096156+RaulMolinaC@users.noreply.github.com>
2020-10-05 17:23:56 +0200

    Contador

---
+++ Test1/Test1/Ejercicios.cs
@@ -0,0 +1,24 @@
+﻿namespace Test1
+{
+    class Ejercicios
+    {
+        // Ejercicio: hacer una función que imprima por
+        // pantalla los númerosdel 0 al 99
+        public static void Ejercicio1()
+        {
+            int contador;
+            contador = 0;
+            while (contador < 100)
+            {
+                System.Console.WriteLine(contador);
+                // contador = contador + 1;
+                // contador += 1;
+                //contador++;
+
+                contador = contador + 1;
+
+            }
+
+        }
+    }
+}
